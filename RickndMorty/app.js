b673ca5a0ba8e83715c9e74f1cefc2c1c9c496b5
Sergio Galindo <s.erickgalindo3@gmail.com>
2023-10-25 12:32:33 -0600

    Proyecto Final

--- RickndMorty/app.js
+++ RickndMorty/app.js
@@ -1,76 +1,71 @@
 import { createCard, createPagination } from "./ui.js";
 
-const pagination= document.querySelector('.pagination');
+const URL = "https://rickandmortyapi.com/api/";
+const pagination = document.querySelector(".pagination");
 
-const URL ="https://rickandmortyapi.com/api/";
-
-
-
-
-async function requestFetch(url){
-    return fetch(url)
-    .then(response => response.json())
-    .then(data =>data)
-    .catch(error => console.log(error))
-} 
-
-async function getCharacters(page = 1){
-   // requestFetch(`${ URL }character/?page= ${ page }`)
-   // .then(data=> {
-   //     showCharacter(data.results);
- //   })
- const data = await requestFetch(`${ URL }character/?page=${ page }`);
- showCharacter(data.resuls);
+async function requestFetch(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function getCharactersById(id, titleModal, bodyModal){
-    requestFetch(`${ URL }character/${ id }`)
-    .then(data=> {
-        titleModal.innerHTML=data.name
-        let htmlBody = `
+async function getCharacters(page = 1) {
+  const data = await requestFetch(`${URL}character/?page=${page}`);
+  showCharacter(data.results);
+}
+
+function getCharactersById(id, titleModal, bodyModal) {
+  requestFetch(`${URL}character/${id}`)
+    .then((data) => {
+      titleModal.innerHTML = data.name;
+      let htmlBody = `
         <img src="${data.image}">
-        <p>${data.species}</p> 
-        <p>${data.origin.name}</p> 
+        <p>${data.species}</p>
+        <p>${data.origin.name}</p>
         `;
-        htmlBody +=data.status=== 'Alive'?
-        <p><span class="badge bg-success">Live</span></p>:
-        <p><span class="badge bg-danger">${ data.status }</span></p>;
-        bodyModal.innerHTML= htmlBody;
-     })
+      htmlBody += data.status === "Alive"
+        ? "<p><span class=\"badge bg-success\">Live</span></p>"
+        : `<p><span class="badge bg-danger">${data.status}</span></p>`;
+      bodyModal.innerHTML = htmlBody;
+    });
 }
 
-function showCharacter(personajes){
-    const contenedor= document.querySelector('.characters');
-    contenedor.innerHTML='';
-    personajes.forEach(personajes => {
-        contenedor.appendChild(createCard(personajes));
-    })
+function showCharacter(characters) {
+  const contenedor = document.querySelector(".characters");
+  contenedor.innerHTML = "";
+  characters.forEach((character) => {
+    contenedor.appendChild(createCard(character));
+  });
 }
 
-
-function buttonAction(e){
-    e.preventDefault()
-    if(e.target.classList.contains('page.link')){
-        const page = e.target.getAttribute('data-id');
-        getCharacters(page);
-    }
+function buttonAction(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("page-link")) {
+    const page = e.target.getAttribute("data-id");
+    getCharacters(page);
+  }
 }
 
+function getButtonCard(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("btn")) {
+    const titleModal = document.querySelector(".modal-title");
+    const bodyModal = document.querySelector(".modal-body");
 
-function getButtonCard(e){
-    e.preventDefault();
-    
-    if(e.target.classList.contains('btn')){
-        const titleModal=document.querySelector('.modal-title');
-        const bodyModal= document.querySelector('.modal-body');
-    
-        titleModal.InnerHTML ='pending...';
-        bodyModal.innerHTML='<i class="fa fa-refresh fa-spin fa-4x"></i>'
-        const id =e.target.getAttribute('data-id');
-        getCharactersById(id, titleModal, bodyModal);
+    titleModal.innerHTML = "pending...";
+    bodyModal.innerHTML = `<i class="fa fa-refresh fa-spin fa-4x"></i>`;
 
-    }
+    const id = e.target.getAttribute("data-id");
+    getCharactersById(id, titleModal, bodyModal);
+  }
 }
 
-pagination.addEventListener('click', buttonAction);
-document.querySelector('.characters').addEventListener('click',getButtonCard)
+pagination.innerHTML = createPagination();
+getCharacters();
+
+document.querySelector(".characters").addEventListener("click", getButtonCard);
+pagination.addEventListener("click", buttonAction);
